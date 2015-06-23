281e96b0b8a460e81c022a08c278d9ff723e8dba
Adrian <adriang_1174@hotmail.com>
2015-06-23 17:02:54 -0400

    version 1.1

---
+++ footer.php
@@ -0,0 +1,52 @@
+	<!-- begin footer -->
+	<footer id="footer">
+    	<div class="container">
+            <!-- begin footer top -->
+            <div id="footer-top">
+                <div class="two-thirds">
+                	<div class="one-half widget contact-info">
+                        	<p class="phone"><strong>Phone:</strong> 4444-4444</p>
+                     </div>
+					 <div class="one-half widget contact-info column-last">
+        	                <p class="email"><strong>Email:</strong> <a href="mailto:info@bpms.com.ar">info@bpms.com.ar</a></p>
+                    </div>
+				</div>
+				<div class="one-third column-last">
+					<div class="widget social-links">
+                            <ul>
+                            	<li class="twitter"><a href="#" title="Twitter" target="_blank">Twitter</a></li>
+                                <li class="facebook"><a href="#" title="Facebook" target="_blank">Facebook</a></li>
+                                <li class="google"><a href="#" title="Google+" target="_blank">Google+</a></li>
+                                <li class="youtube"><a href="#" title="YouTube" target="_blank">YouTube</a></li>
+                                <li class="skype"><a href="#" title="Skype" target="_blank">Skype</a></li>
+                                <li class="rss"><a href="#" title="RSS" target="_blank">RSS</a></li>
+                            </ul>
+                    </div>
+                    
+                </div>
+            </div>
+            <!-- end footer top -->
+
+            <!-- begin footer bottom -->
+            <div id="footer-bottom">
+            	<div class="one-half">
+                	<p>Copyright &copy; 2014.</p>
+                </div>
+
+                <div class="one-half column-last">
+                	<nav id="footer-nav">
+                        <ul>
+                            <li><a href="index.html">Home</a> &middot;</li>
+                            <li><a href="about-us.html">Templates</a> &middot;</li>
+                            <li><a href="elements.html">Features</a> &middot;</li>
+                            <li><a href="portfolio.html">Portfolio</a> &middot;</li>
+                            <li><a href="blog.html">Blog</a> &middot;</li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!-- end footer bottom -->
+        </div>
+	</footer>
+	<!-- end footer -->
---
+++ header.php
@@ -0,0 +1,81 @@
+	<!-- begin header -->
+    <header id="header" class="container">
+        <!-- begin header top -->
+        <section id="header-top" class="clearfix">
+            <!-- begin header left -->
+            <div class="one-fifth">
+                <h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="BPMS"></a></h1>
+                <p id="tagline"></p>
+                <div id="fondo-logo">
+                </div>
+            </div> 
+            <!-- end header left -->
+            
+            <!-- begin header right -->
+            <div class="four-fifths column-last">
+                <!-- begin language switcher -->
+                <!-- end language switcher -->
+                
+                <!-- begin contact info -->
+                <div class="contact-info">
+                    <!--<p class="phone"> 4444-4444</p> -->
+                    <p class="email"><a href="mailto:info@finesse.com">info@bpms.com.ar</a></p>
+                </div>
+                <!-- end contact info -->
+                <div class="nav-bar">
+                <!-- begin navigation bar -->
+						<section id="navbar" class="clearfix">
+
+							<!-- begin navigation -->
+							<nav id="nav">
+							  <ul id="navlist" class="clearfix">
+									<li><a href="bpms_solutions.html" data-rel="submenu2">Industrias</a>
+										<ul id="submenu2" class="ddsubmenustyle">
+											<li><a href="financial_services.html">Servicios Financieros</a></li>
+											<li><a href="insurance.html">Seguros</a></li>
+											<li><a href="health.html">Salud</a></li>							
+											<li><a href="comm.html">Comunicaciones & Media</a></li>							
+											<li><a href="government.html">Gobierno</a></li>							
+											<li><a href="transportation.html">Automotrices & Transporte</a></li>							
+											<li><a href="manufacturing.html">Manufactura</a></li>							
+										</ul>
+									<li><a href="bpms_services.html" data-rel="submenu3">Servicios</a>
+										<ul id="submenu3" class="ddsubmenustyle">
+											<li><a href="services.html">Servicios Profesionales</a></li>
+											<li><a href="support.html">Soporte</a></li>	
+										</ul>
+									</li>
+									<li><a href="proximamente.html">Productos</a>
+									<li><a href="bpms_library.html">Biblioteca</a>
+									</li>
+									<li><a href="blog.html" data-rel="submenu5">Acerca de BPMS</a>
+										<ul id="submenu5" class="ddsubmenustyle">
+											<li><a href="history.html">Historia</a></li>
+											<li><a href="team.html">Equipo</a></li>
+										</ul>
+									</li>
+									<li><a href="contact.html">Contacto</a></li>
+								</ul>
+							</nav>
+							<!-- end navigation -->
+			
+							<!-- begin search form -->
+							<form id="search-form" action="search.php" method="get">
+								<input id="s" type="text" name="s" style="display: none;">
+								<input id="search-submit" type="submit" name="search-submit" value="Search">
+							</form>
+							<!-- end search form -->
+						</section>
+						<!-- end navigation bar -->
+					</div>
+            </div>
+            
+            <!-- end header right -->
+        </section>
+        <!-- end header top -->
+
+
+<!-- Aca iba navbar -->
+
+    </header>
+    <!-- end header -->
